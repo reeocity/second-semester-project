560b1337906a9a5123dda76710e99e79155aeeba
reeocity <ibraheemreedwan100@gmail.com>
2024-12-31 21:59:21 +0100

    fixed blogroutes

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -5,14 +5,13 @@ const { authMiddleware } = require('../middlewares/authMiddleware');
 const {
     createBlog,
     getBlogs,
-    getBlogById,
-    updateBlogState,
+    getBlogById,    
 } = require('../controllers/blogController');
 
 
 router.post('/', authMiddleware, createBlog);
 router.get('/', getBlogs);
 router.get('/:id', getBlogById);
-router.patch('/:id/state', authMiddleware, updateBlogState);
+
 
 module.exports = router;
